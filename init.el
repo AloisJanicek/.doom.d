8d7510142e97cade8f4da73a8b3cb30dc1ee047e
Alois Janíček <janicek.dev@gmail.com>
2018-06-05 17:45:12 +0200

    update doom! call to latest standard

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 ;;; init.el -*- lexical-binding: t; -*-
 ;;
 ;; doom! call
-;;
-;; Copy me to ~/.doom.d/init.el or ~/.config/doom/init.el, then edit me!
 (doom! :feature
       ;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
@@ -13,19 +11,21 @@
         +docsets)        ; ...or in Dash docsets locally
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
-       (syntax-checker
-        +childframe); tasing you for every semicolon you forget
+       (syntax-checker   ; tasing you for every semicolon you forget
+        +childframe)     ; use childframes for error popups (Emacs 26+ only)
        version-control   ; remember, remember that commit in November
        workspaces        ; tab emulation, persistence & separate workspaces
 
        :completion
        (company          ; the ultimate code completion backend
         +auto            ; as-you-type code completion
-        +childframe)     ; a nicer company UI (Emacs 26+ only)
-      ;helm              ; the *other* search engine for love and life
+        +childframe)
+      ;(helm             ; the *other* search engine for love and life
+      ; +fuzzy)          ; enable fuzzy search backend for helm
       ;ido               ; the other *other* search engine...
        (ivy              ; a search engine for love and life
-        +childframe)     ; uses childframes for popups (Emacs 26+ only)
+        +fuzzy           ; enable fuzzy search backend for ivy
+        +childframe)
 
        :ui
        doom              ; what makes DOOM look the way it does
@@ -50,67 +50,67 @@
        electric-indent   ; smarter, keyword-based electric-indent
        eshell            ; a consistent, cross-platform shell (WIP)
        imenu             ; an imenu sidebar and searchable code index
-      term              ; terminals in Emacs
+       term              ; terminals in Emacs
 
        :tools
        editorconfig      ; let someone else argue about tabs vs spaces
       ;ein               ; tame Jupyter notebooks with emacs
-      gist              ; interacting with github gists
+       gist              ; interacting with github gists
       ;macos             ; MacOS-specific commands
       ;make              ; run make tasks from Emacs
-      magit             ;
+       magit             ;
       ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
-      prodigy           ; FIXME managing external services & code builders
-      rgb               ; creating color strings
+       prodigy           ; FIXME managing external services & code builders
+       rgb               ; creating color strings
        rotate-text       ; cycle region at point between text candidates
        tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
 
        :lang
-       assembly          ; assembly for fun or debugging
-       ;; cc                ; C/C++/Obj-C madness
-       ;; crystal           ; ruby at the speed of c
-       ;; clojure           ; java with a lisp
-       ;; csharp            ; unity, .NET, and mono shenanigans
+      ;assembly          ; assembly for fun or debugging
+      ;cc                ; C/C++/Obj-C madness
+      ;crystal           ; ruby at the speed of c
+      ;clojure           ; java with a lisp
+      ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
-       ;; elixir            ; erlang done right
-       ;; elm               ; care for a cup of TEA?
+      ;elixir            ; erlang done right
+      ;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
-       ;; ess               ; emacs speaks statistics
-       ;; go                ; the hipster dialect
-       ;; (haskell +intero) ; a language that's lazier than I am
-       ;; hy                ; readability of scheme w/ speed of python
-       ;; (java +meghanada) ; the poster child for carpal tunnel syndrome
+      ;ess               ; emacs speaks statistics
+      ;go                ; the hipster dialect
+      ;(haskell +intero) ; a language that's lazier than I am
+      ;hy                ; readability of scheme w/ speed of python
+      ;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       ;; julia             ; a better, faster MATLAB
-       ;; latex             ; writing papers in Emacs has never been so fun
-       ;; ledger            ; an accounting system in Emacs
-       ;; lua               ; one-based indices? one-based indices
+      ;julia             ; a better, faster MATLAB
+      ;latex             ; writing papers in Emacs has never been so fun
+      ;ledger            ; an accounting system in Emacs
+      ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
       ;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
-       ;; ocaml             ; an objective camel
+      ;nix               ; I hereby declare "nix geht mehr!"
+      ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        +present         ; Emacs for presentations
-        )                 ; Emacs+Org as a static site generator
-       perl              ; write code no one else can comprehend
-       ;; php               ; perl's insecure younger brother
-       ;; plantuml          ; diagrams for confusing people more
-       ;; purescript        ; javascript, but functional
-       ;; python            ; beautiful is better than ugly
+        +present)        ; Emacs for presentations
+      ;perl              ; write code no one else can comprehend
+      ;php               ; perl's insecure younger brother
+      ;plantuml          ; diagrams for confusing people more
+      ;purescript        ; javascript, but functional
+       python            ; beautiful is better than ugly
+      ;qt                ; the 'cutest' gui framework ever
        rest              ; Emacs as a REST client
-       ;; ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;; rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;; scala             ; java, but good
+      ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+      ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+      ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
-       ;; solidity          ; do you need a blockchain? No.
-       ;; swift             ; who asked for emoji variables?
+      ;solidity          ; do you need a blockchain? No.
+      ;swift             ; who asked for emoji variables?
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
@@ -122,22 +122,23 @@
       ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
       (write            ; emacs as a word processor (latex + org + markdown)
-      +wordnut         ; wordnet (wn) search
-      +langtool)       ; a proofreader (grammar/style check) for Emacs
+       +wordnut         ; wordnet (wn) search
+       +langtool)       ; a proofreader (grammar/style check) for Emacs
 
        :collab
-       ;; floobits          ; peer programming for a price
-      impatient-mode    ; show off code over HTTP
+      ;floobits          ; peer programming for a price
+      ;impatient-mode    ; show off code over HTTP
+
        :config
+       ;; For literate config users. This will tangle+compile a config.org
+       ;; literate config in your `doom-private-dir' whenever it changes.
+      ;literate
+
        ;; The default module set reasonable defaults for Emacs. It also provides
        ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
        ;; and additional ex commands for evil-mode. Use it as a reference for
        ;; your own modules.
-       (default +bindings +snippets +evil-commands)
-       :private ;; ~/.doom.d/modules/private/{...}
-       ;; python
-       )
-
+       (default +bindings +snippets +evil-commands))
 ;;
 ;; Global variables
 ;;
