e979e3a13ee2f1899eb68f9a62d47d36f9120171
Alois Janíček <janicek.dev@gmail.com>
2018-06-09 22:05:21 +0200

    customize same faces

--- init.el
+++ init.el
@@ -206,14 +206,17 @@
 (setq +doom-quit-messages '(""))
 ;; scratch: inherit major mode from latest active buffer
 (setq doom-scratch-buffer-major-mode t)
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(show-paren-match-expression ((t (:background "SkyBlue4"))))
-;;  '(web-mode-current-element-highlight-face ((t (:foreground "#51afef"))))
-;;  '(web-mode-html-attr-equal-face ((t (:foreground "#5B6268"))))
-;;  '(web-mode-html-tag-bracket-face ((t (:foreground "#5B6268"))))
-;;  '(web-mode-html-tag-face ((t (:foreground "#E06C75"))))
-;;  '(web-mode-html-tag-unclosed-face ((t (:inherit web-mode-html-tag-face :underline (:color "#ff6c6b" :style wave))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match-expression ((t (:background "SkyBlue4"))))
+ '(Man-overstrike ((t (:foreground "#ff7a79"))))
+ '(Man-underline ((t (:inherit underline :foreground "#98be65"))))
+ '(fixed-pitch-serif ((t (:family "Iosevka Slab"))))
+ '(web-mode-current-element-highlight-face ((t (:foreground "#51afef"))))
+ '(web-mode-html-attr-equal-face ((t (:foreground "#5B6268"))))
+ '(web-mode-html-tag-bracket-face ((t (:foreground "#5B6268"))))
+ '(web-mode-html-tag-face ((t (:foreground "#E06C75"))))
+ '(web-mode-html-tag-unclosed-face ((t (:inherit web-mode-html-tag-face :underline (:color "#ff6c6b" :style wave))))))
