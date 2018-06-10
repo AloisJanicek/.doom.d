5865f5f437e11caae138acd4713e98423f32731f
Alois Janíček <janicek.dev@gmail.com>
2018-06-10 13:27:57 +0200

    remove old comment line

--- +bindings.el
+++ +bindings.el
@@ -75,7 +75,6 @@
    )
  (:after org-capture
    (:map org-capture-mode-map
-     ;; Switch to other workspaces even if org-capture is opened and focused
      :inve [escape]       #'org-capture-finalize
      (:localleader
        :desc "Schedule"     :nv "s" #'org-schedule
