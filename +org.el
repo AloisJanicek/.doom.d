dd4eb456692a3fd1b19dce59917b02fa5c60dc67
Alois Janíček <janicek.dev@gmail.com>
2018-06-17 11:38:55 +0200

    lets try GTD this way

--- +org.el
+++ +org.el
@@ -114,7 +114,7 @@
    ;;           todo     ongoing  hold         zap      done
    '((sequence "[ ](t)" "[-](o)" "[!](h)" "|" "[✘](z)" "[✔](d)")
      (sequnece "STARTED(s)" "|" "FINISHED(f)")
-     (sequence "TODO(T)" "NEXT(n)" "WAITING(w)" "LATER(l)" "|" "DONE(D)" "CANCELLED(c)"))
+     (sequence "MAYBE(M)" "SOMEDAY(S)" "TODO(T)" "NEXT(n)" "WAITING(w)" "LATER(l)" "|" "DONE(D)" "CANCELLED(c)"))
    org-todo-keyword-faces '(("NEXT" . "#98be65") ("WAITING" . "#c678dd") ("TODO" . "#ECBE7B") ("STARTED" . "#4db5bd"))
    org-enforce-todo-dependencies t
    org-enforce-todo-checkbox-dependencies nil
