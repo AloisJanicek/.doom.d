1be2509ede679df7c5b91c405e478b1122d15f46
Alois Janíček <janicek.dev@gmail.com>
2018-06-22 20:19:00 +0200

    further project navigation improvements
    * I don't bootstrap project that often, so it can live on
    capital B
    * projectile switch to buffer could be useful

--- +bindings.el
+++ +bindings.el
@@ -386,9 +386,10 @@
       )
     (:desc "project"    :prefix                "p"
       :desc "Agenda"                   :nv     "a" #'aj/project
-      :desc "bootstrap"                :nv     "b" #'aj/project-bootstrap
+      :desc "bootstrap"                :nv     "B" #'aj/project-bootstrap
       :desc "directories"              :nv     "d" #'counsel-projectile-find-dir
       :desc "files"                    :nv     "f" #'counsel-projectile-find-file
+      :desc "buffer"                   :nv     "b" #'counsel-projectile-switch-to-buffer
       :desc "Capture"                  :nv     "x" #'aj/org-projectile-capture-for-current-project
       :desc "P README"                 :nv     "p" #'aj/better-open-current-projectile-org-file
       :desc "Switch"                   :nv     "s" #'counsel-projectile-switch-project
