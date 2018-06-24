94e64ad2cad65a8aea2606453088aad3b6c63746
Alois Janíček <janicek.dev@gmail.com>
2018-06-24 21:46:50 +0200

    lang/racket: essential key bindings

--- modules/lang/racket/config.el
+++ modules/lang/racket/config.el
@@ -4,8 +4,119 @@
   :config
   (set-repl-handler! 'racket-mode #'+racket/repl)
   (set-popup-rule! "*Racket REPL\*" :quit nil :ttl nil :side 'bottom :size 0.4)
-  )
+  (map!
+   :map racket-mode-map
+   :localleader
 
+   ;; when in normal mode, evaluate whole buffer
+   :desc    "run"                   :n    "e"     #'racket-run
+
+   ;; otherwise send region to REPL
+   :desc    "run/send-region"           :v    "e"     #'racket-send-region
+   :desc    "send-definition"       :nv   "d"     #'racket-send-definition
+   :desc    "send-last-sexp"        :nv   "s"     #'racket-send-last-sexp
+
+   ;; require offline racket documentation
+   ;; settings:
+   ;; open =drracket=
+   ;; Preferences -> Browsers -> [/usr/bin/xdg-open " ] <URL> ["     ]
+   :desc    "doc"                   :nv   "h"     #'racket-doc
+   ;; code manipulation -------------
+   ;; racket-backward-up-list
+   ;; racket-cycle-paren-shapes
+   ;; racket-unalign
+   ;; racket-align
+
+   ;; language tooling -------------
+   ;; racket-base-requires
+   ;; racket-find-collection
+
+
+   ;; racket-describe-mode
+   ;; racket-describe
+   ;; racket-describe--next-button ;; evilize it
+   ;; racket-describe--prev-button ;; evilize it
+
+   ;; mode specific ----------------
+   ;; racket-bug-report
+
+   ;; syntax check mode ------------------ evilize it
+   ;; racket-check-syntax-mode
+   ;; racket-check-syntax-mode-goto-def
+   ;; racket-check-syntax-mode-goto-next-def
+   ;; racket-check-syntax-mode-goto-next-use
+   ;; racket-check-syntax-mode-goto-prev-def
+   ;; racket-check-syntax-mode-goto-prev-use
+   ;; racket-check-syntax-mode-help
+   ;; racket-check-syntax-mode-quit
+   ;; racket-check-syntax-mode-rename
+
+   ;; expand region/send
+   ;; racket-expand-again
+   ;; racket-expand-definition
+   ;; racket-expand-last-sexp
+   ;; racket-expand-region
+
+   ;; tests
+   ;; racket-test
+   ;; racket-raco-test
+   ;; racket-fold-all-tests
+   ;; racket-unfold-all-tests
+
+
+   ;; probably don't need those
+   ;; racket-indent-line
+   ;; racket-insert-closing
+   ;; racket-insert-lambda
+   ;; racket-smart-open-bracket
+
+   ;; loger
+   ;; racket-logger
+   ;; racket-logger-clear
+   ;; racket-logger-exit
+   ;; racket-logger-mode
+   ;; racket-logger-mode-menu
+   ;; racket-logger-next-item
+   ;; racket-logger-previous-item
+   ;; racket-logger-topic-level
+
+   ;; racket-mode
+   ;; racket-mode-menu
+
+   ;; racket-profile-mode
+   ;; racket-profile
+
+   ;; run current file in shell with racket <file.rkt>
+   ;; racket-racket
+
+   ;; racket-repl-mode
+   ;; racket-repl
+   ;; racket-repl--clean-image-cache
+   ;; racket-repl-eval-or-newline-and-indent
+   ;; racket-repl-switch-to-edit
+   ;; racket-repl-mode-menu
+
+   ;; racket-run
+   ;; racket-run-and-switch-to-repl
+   ;; racket-run-with-errortrace
+
+   ;; requires -----------
+   ;; racket-tidy-requires
+   ;; racket-trim-requires
+
+   ;; racket-open-require-path
+
+   ;; racket-unicode-input-method-enable
+
+   ;; racket-view-last-image
+
+   ;; visit
+   ;; racket-visit-definition
+   ;; racket-visit-module
+   ;; racket-unvisit
+   )
+  )
+;; (setq +eval-runners nil)
 (def-package! ob-racket
   :after (racket-mode org)
   :config
