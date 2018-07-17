4675c51261cf190126363b76df7fcf9215bce56c
Alois Janíček <janicek.dev@gmail.com>
2018-07-17 11:39:32 +0200

    don't close opened info with outside ESC

--- autoload/functions.el
+++ autoload/functions.el
@@ -146,8 +146,8 @@ Use `org-agenda-refile' in `org-agenda' mode."
 (defun aj/set-info-popup-width (&optional asdf asds)
   "Set width of info popup buffer"
   (if doom-big-font-mode
-      (set-popup-rule! "*info*"                         :size 0.6 :side 'left :select t :transient nil)
-    (set-popup-rule! "*info*"                         :size 0.4 :side 'left :select t :transient nil)
+      (set-popup-rule! "*info*"                         :size 0.6 :side 'left :select t :transient nil :quit nil)
+    (set-popup-rule! "*info*"                         :size 0.4 :side 'left :select t :transient nil :quit nil)
     ))
 ;;;###autoload
 (defun my-web-mode-hook ()
