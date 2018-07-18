77bdaaf632fb21b15a18476cafd4ad946f5ea785
Alois Janíček <janicek.dev@gmail.com>
2018-07-18 11:20:24 +0200

    this is ok, problem is elsewhere

--- config.el
+++ config.el
@@ -481,7 +481,7 @@
   (add-hook 'js2-mode-hook (lambda () (setq-local counsel-dash-docsets '("JavaScript" "HTML" "CSS"))))
   (add-hook 'js2-mode-hook 'eslintd-fix-mode)
   (setq-default indent-tabs-mode nil)
-  ;; (setq-default js2-basic-offset 2)
+  (setq-default js2-basic-offset 2)
   )
 
 (after! json-mode
